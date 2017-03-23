be4211d6a50ac5de97747c62ddeb5c43659b8a64
swaminathan.42@osu.edu <swaminathan.42@osu.edu>
2017-03-23 00:16:39 -0400

    Fixing issues with the arrow head and activating countries

--- midterm/network/network.js
+++ midterm/network/network.js
@@ -8,6 +8,18 @@ window.Network = (() => {
 		return (d) => radius_calculator(d) + 13
 	}
 
+	var deactivate_all_nodes = function (clicked_node_class_name) {
+		var activated_node = $(".activated")
+		var clicked_node_active_now = (activated_node.attr("class") || "").split(/\s+/).some((x) => x === clicked_node_class_name)
+		var currently_active_elements = $(".active")
+		$(".link.active").attr("marker-end", "url(#arrow)")
+		currently_active_elements.removeClass("in")
+		currently_active_elements.removeClass("out")
+		activated_node.removeClass("activated")
+		currently_active_elements.removeClass("active")
+		$(".subdued").removeClass("subdued")
+		return clicked_node_active_now
+	}
 	var Network = function (chartId, nodes, links, width, height, radius_range) {
 		this.left_padding = 100
 		this.right_padding = 100
@@ -71,31 +83,29 @@ window.Network = (() => {
 				.data(nodes, (x) => x["BLocUID"])
 				.enter()
 					.append("g")
-						.attr("class", (d)=>d.classes + " node-group")
+						.attr("class", (d)=>d.classes + " " + d.class_name+ " "+ " node-group")
 					.on("click", function (node) {
+						var is_node_already_active = deactivate_all_nodes(node.class_name)
 						var class_name = node.class_name
-						node.activated = !node.activated
-						obj.chart.selectAll(".t_"+class_name).classed("active out", node.activated)
-						obj.chart.selectAll(".s_"+class_name).classed("active in", node.activated)
-						obj.chart.selectAll(".link.active.in").attr("marker-end", node.activated ? "url(#arrow_active_in)" : "url(#arrow)")
-						obj.chart.selectAll(".link.active.out").attr("marker-end", node.activated ? "url(#arrow_active_out)" : "url(#arrow)")
-						obj.chart.selectAll("."+class_name).classed("activated", node.activated)
-						if (node.activated) {
-							$("#"+obj.chartId +" :not(.active,.activated)").addClass("subdued")
-						} else {
-							$("#"+obj.chartId +" :not(.active,.activated)").removeClass("subdued")
+						if (!is_node_already_active) {
+							obj.chart.selectAll(".t_"+class_name).classed("active out", true)
+							obj.chart.selectAll(".s_"+class_name).classed("active in", true)
+							obj.chart.selectAll(".link.active.in").attr("marker-end", "url(#arrow_active_in)")
+							obj.chart.selectAll(".link.active.out").attr("marker-end", "url(#arrow_active_out)")
+							obj.chart.selectAll("."+class_name).classed("activated", true)
+							$("#"+obj.chartId +" :not(defs,marker,marker>path,.active,.activated)").addClass("subdued")
 						}
 					})
 			this.node
 				.append("circle")
-					.attr("class", (d) => d.classes + " node " + (d.is_attractor ? " attractor" : ""))
+					.attr("class", (d) => d.classes + " " + d.class_name+ " " + " node " + (d.is_attractor ? " attractor" : ""))
 					.attr("r", this.radius_calculator)
 					.attr("fill", color_calculator)
 
 			this.node.append("text")
 				.attr("dx", 0)
 				.attr("dy", this.label_pos_calculator)
-				.attr("class", (d) => d.classes + " text-label " + (d.small_label ? " small " : (d.is_attractor ? " attractor " : "medium")))
+				.attr("class", (d) => d.classes + " " + d.class_name+ " "+ " text-label " + (d.small_label ? " small " : (d.is_attractor ? " attractor " : "medium")))
 				.text((d) => d.BLCountry)
 			this.simulation.nodes(nodes)
 			this.simulation.force("link").links(links)
